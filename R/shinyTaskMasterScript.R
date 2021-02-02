632addeb53830338babe365710dd70346b630390
AarshBatra <aarshbatra.in@gmail.com>
2021-02-03 05:11:18 +0530

    Setting up repository: First Commit

---
+++ R/shinyTaskMasterScript.R
@@ -0,0 +1,26 @@
+## R Shiny Data Task===========================================================
+
+# metadata---------------------------------------------------------------------
+# author: Aarsh Batra
+# date: February 2, 2021
+# R version: 4.0.3 (2020-10-10)
+# Platform: x86_64-w64-mingw32/x64 (64-bit)
+# Running under: Windows 10 x64 (build 18363)
+# R Studio version: 1.4.1103
+
+# libraries--------------------------------------------------------------------
+library(tidyverse)
+library(knitr)
+library(devtools)
+library(stringr)
+library(tidyr)
+library(dplyr)
+library(magrittr)
+library(data.table)
+library(lubridate)
+library(roxygen2)
+library(testthat)
+library(shiny)
+library(ggplot2)
+library(leaflet)
+
