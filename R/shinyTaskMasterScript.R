e717ca7fa71cb6c3f5cdc14871c0e643bb0295e3
AarshBatra <aarshbatra.in@gmail.com>
2021-02-03 17:19:23 +0530

    testing devtools document function, status: Working without errors

--- R/shinyTaskMasterScript.R
+++ R/shinyTaskMasterScript.R
@@ -35,6 +35,7 @@ library(geodist) # for calculating distances given Lat and Long
 #'
 #'
 #' @importFrom readr read_csv
+#' @importFrom base file.exists stop
 #' @param path_to_file path to the file where the data is stored
 #'
 #' @return This function returns an R object which contains the raw dataset.
@@ -54,8 +55,8 @@ read_raw_data <- function(path_to_file){
     stop("file not present in the specified location. Please make sure
          that you point the path to the raw data csv file.")
   }
-  raw_ships_data <- readr::read_csv(file = path_to_file)
-  raw_ships_data
+  raw_data <- readr::read_csv(file = path_to_file)
+  raw_data
 }
 
 
@@ -64,30 +65,30 @@ read_raw_data <- function(path_to_file){
 #' cleaning and rearranging Raw Data for analysis
 #'
 #' This function takes in the raw dataset and does some basic cleaning (if any)
-#' rearranging (if any). As of now, it makes sure that the dates are in
-#' chronological order.
+#' rearranging (if any).
 #'
-#' @importFrom dplyr arrange
-#' @param raw_data raw dataset that is returned by the \code{read_data}
+#' @importFrom dplyr arrange select
+#' @param raw_data raw dataset that is returned by the \code{read_raw_data}
 #'        function
-#'
-#' @note  raw dataset is already almost clean but if any rearranging/cleaning
-#'        is needed before analysis, it will be done via this function.
 #'
 #' @return this function returns the cleaned dataset which is ready
 #'         for analysis.
 #'
 #' @examples
-#' clean_raw_ships_data(raw_data = raw_dataset)
+#' clean_raw_data(raw_data = raw_dataset)
 #'
 #' @export
 
 
-clean_raw_ships_data <- function(raw_data){
+clean_raw_data <- function(raw_data){
 
   # rearrange dates into chronological order
-  cleaned_ships_data <- dplyr::arrange(raw_data, DATETIME)
-  cleaned_ships_data
+  cleaned_data <- dplyr::arrange(raw_data, DATETIME)
+
+  # rearrange columns such that LON column is the first column
+  cleaned_data <- dplyr::select(cleaned_data, LON, everything())
+
+  cleaned_data
 }
 
 
@@ -96,28 +97,32 @@ clean_raw_ships_data <- function(raw_data){
 
 
 # exploring: raw dataset-------------------------------------------------------
-raw_ships_data <- read_raw_data(path_to_file = "data-raw/ships.csv")
-dim(raw_ships_data)
-length(unique(raw_ships_data$SHIP_ID))
-base::summary(raw_ships_data)
+raw_data <- read_raw_data(path_to_file = "data-raw/ships.csv")
+dim(raw_data)
+length(unique(raw_data$SHIP_ID))
+base::summary(raw_data)
 
 
 # exploring: cleaned dataset---------------------------------------------------
-cleaned_ships_data <- clean_raw_ships_data(raw_ships_data)
+cleaned_data <- clean_raw_data(raw_data)
 
-View(cleaned_ships_data)
+View(cleaned_data)
 
-filter_cleaned_ships_data <- function(ship_type = "cargo", ship_name = "MERI"){
-  filtered_cleaned_ships_data <- cleaned_ships_data %>%
+filter_cleaned_data <- function(cleaned_data, ship_type = "cargo",
+                                ship_name = "MERI"){
+  filtered_cleaned_data <- cleaned_data %>%
     dplyr::filter(ship_type == ship_type,
                   SHIPNAME == ship_name)
-  filtered_cleaned_ships_data
+  filtered_cleaned_data
 }
 
-filtered_cleaned_ships_data <- filter_cleaned_ships_data(cleaned_ships_data)
-
-
-
+filtered_cleaned_data <- filter_cleaned_data(cleaned_data)
+#
+# foo <- geodist(cleaned_ships_data[1, 2:1], cleaned_ships_data[2, 2:1],
+#         measure = "geodesic")
+# foo/1000
+#
+# geodist(foo, sequential = TRUE, measure = "haversine")/1000
 
 
 
