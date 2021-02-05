af225eae9ba2a61afcde2cd7b7e7dc8fb27cfea6
AarshBatra <aarshbatra.in@gmail.com>
2021-02-05 16:30:35 +0530

    app.R bug removed, second dropdown filtered given first dropdown selection, next to do: add leaflet map

--- app.R
+++ app.R
@@ -4,73 +4,51 @@ library(shiny.semantic)
 # making a marine data module
 marine_ui <- function(id){
   shiny.semantic::semanticPage(
+    title = "Marine Data Visualization",
 
     tagList(
-      dropdown_input(NS(id, "ship_type_dropdown"),
-                     unique(cleaned_data$ship_type), value = "Tanker"),
-      p("Selected ship type:"),
-      textOutput(NS(id, "dropdown_ship_type_selected")),
 
-      dropdown_input(NS(id, "ship_name_dropdown"),
-                     unique(cleaned_data$SHIPNAME),  value = "MARINUS"),
+      shiny::tags$h3("Ship Type"),
+      shiny.semantic::dropdown_input(NS(id, "ship_type_dropdown"),
+                                     unique(cleaned_data$ship_type)),
 
-      p("Selected Ship Name:"),
-      textOutput(NS(id, "dropdown_ship_name_selected")),
-      tags$br(),
-      textOutput(NS(id, "test")),
+      shiny::tags$br(),
+      shiny::tags$h3("Ship Name"),
+      shiny::uiOutput(NS(id, "ship_name_dropdown")),
 
-      leafletOutput(NS(id, "leaflet"))
+
+
+      leaflet::leafletOutput(NS(id, "leaflet"))
     )
 
   )
 }
 
 
-marine_server <- function(id, df){
+marine_server <- function(id){
   shiny::moduleServer(id, function(input, output, session){
-    output$dropdown_ship_type_selected <- renderText({
-      input$ship_type_dropdown
-    })
 
-    ship_type <- reactive({input$ship_type_dropdown})
+    output$ship_name_dropdown <- renderUI({
 
-    output$dropdown_ship_name_selected <- renderText({
-      input$ship_name_dropdown
-    })
-
-    updatedData <- reactive({
-      filter(df(), ship_type == ship_type()) # nrow(df()) works
-    })
-
-
-    output$test <- renderText({
-      paste(updatedData()$SHIPNAME) # paste(updatedData()) works
-    })
-
-    output$leaflet <- renderLeaflet({
+      ns <- session$ns
+      shiny.semantic::dropdown_input(ns("ship_name_dd_selected"),
+                                     unique(cleaned_data[
+    (cleaned_data$ship_type == input$ship_type_dropdown), "SHIPNAME"]))
 
     })
-
-    # update_dropdown_input(session, "ship_name_dropdown",
-    #                       choices = updatedData()$SHIPNAME, value = "foo")
-
-    # observeEvent(input$ship_type_dropdown, {
-    #   updated_data <- dplyr::filter(data(), ship_type == input$ship_type_dropdown)
-    #   choices <- updated_data$SHIPNAME
-    #   update_dropdown_input(session, "ship_name_dropdown",
-    #             choices = choices, value = input$ship_name_dropdown)
-    #
-    # })
 
   })
+
 }
+
+
 
 ui <- semanticPage(
   marine_ui("m1")
 )
 
 server <- function(input, output, session)(
-  marine_server("m1", reactive({cleaned_data}))
+  marine_server("m1")
 )
 
 shiny::shinyApp(ui = ui, server = server)
